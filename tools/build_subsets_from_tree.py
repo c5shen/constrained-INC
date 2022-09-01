709eee66c1316a24e5693f110d0f016c1c71c2a7
Shen <chengze5@golubh4.campuscluster.illinois.edu>
2022-09-01 13:27:53 -0500

    some fixes according to Min

--- tools/build_subsets_from_tree.py
+++ tools/build_subsets_from_tree.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
 """
 Build subsets using centroid decomposition from PASTA
--- tools/compare_trees.py
+++ tools/compare_trees.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 """
 Robust comparison of two trees using dendropy.
 
